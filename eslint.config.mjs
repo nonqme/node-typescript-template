9e19faa448d39b4624a32f8130d3c489e6516f94
nonqme <w.missary@gmail.com>
2024-08-05 22:29:35 +0200

    chore: add prettier plugin for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import nodePlugin from 'eslint-plugin-n';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
@@ -9,12 +10,19 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   nodePlugin.configs['flat/recommended'],
+  eslintConfigPrettier,
   {
     rules: {
       'n/no-unpublished-import': [
         'error',
         {
-          allowModules: ['globals', '@eslint/js', 'eslint-plugin-n', 'typescript-eslint'],
+          allowModules: [
+            'globals',
+            '@eslint/js',
+            'eslint-plugin-n',
+            'typescript-eslint',
+            'eslint-config-prettier',
+          ],
         },
       ],
     },
