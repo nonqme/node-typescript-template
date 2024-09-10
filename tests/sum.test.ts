34ff727629d47a42f87cc3911bb5c61737e75fcd
nonqme <w.missary@gmail.com>
2024-09-10 18:23:34 +0200

    feat: add type module to typescript

--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -1,7 +1,7 @@
 import test from 'node:test';
 import assert from 'node:assert';
 
-import { sum } from '../src/index';
+import { sum } from '../src/index.js';
 
 test('adds 1 + 2 to equal 3', () => {
   const result: number = sum(1, 2);
